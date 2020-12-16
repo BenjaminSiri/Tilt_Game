0f352882778f00ac2b1e63c6bc93af338aeb2a96
Ben <ben@acsiri.com>
2020-12-16 13:00:53 -0500

    Add platform and started movement

---
+++ Tilt game/Assets/Scripts/PlaneMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneMovement : MonoBehaviour
+{
+    [Header("Movement")]
+    [SerializeField] KeyCode rotateLeft;
+    [SerializeField] KeyCode rotateRight;
+    [SerializeField] KeyCode rotateUp;
+    [SerializeField] KeyCode rotateDown;
+    [SerializeField] float rotationSpeed = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(rotateUp))
+        {
+            transform.Rotate(Vector3.right * (rotationSpeed * Time.deltaTime));
+        }
+        if (Input.GetKey(rotateDown))
+        {
+            transform.Rotate(Vector3.left * (rotationSpeed * Time.deltaTime));
+        }
+        if (Input.GetKey(rotateRight))
+        {
+            transform.Rotate(Vector3.back * (rotationSpeed * Time.deltaTime));
+        }
+        if (Input.GetKey(rotateLeft))
+        {
+            transform.Rotate(Vector3.forward * (rotationSpeed * Time.deltaTime));
+        }
+
+    }
+}
