4d0b11fe441bb523615fd821602bdd4f01860759
Ben <ben@acsiri.com>
2020-12-16 16:26:19 -0500

    progress

--- Tilt game/Assets/Scripts/PlaneMovement.cs
+++ Tilt game/Assets/Scripts/PlaneMovement.cs
@@ -10,6 +10,7 @@ public class PlaneMovement : MonoBehaviour
     [SerializeField] KeyCode rotateUp;
     [SerializeField] KeyCode rotateDown;
     [SerializeField] float rotationSpeed = 1.0f;
+    [SerializeField] float maxRotation = 20.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -20,20 +21,25 @@ public class PlaneMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(rotateUp))
+        Vector3 eulerRotation = transform.rotation.eulerAngles;
+        transform.rotation = Quaternion.Euler(eulerRotation.x, 0, eulerRotation.z);
+
+        if (Input.GetKey(rotateUp) && !(transform.rotation.eulerAngles.x > 20 && transform.rotation.eulerAngles.x < 338))
         {
             transform.Rotate(Vector3.right * (rotationSpeed * Time.deltaTime));
         }
-        if (Input.GetKey(rotateDown))
+        if (Input.GetKey(rotateDown) && !(transform.rotation.eulerAngles.x > 22 && transform.rotation.eulerAngles.x < 340))
         {
             transform.Rotate(Vector3.left * (rotationSpeed * Time.deltaTime));
         }
-        if (Input.GetKey(rotateRight))
+        if (Input.GetKey(rotateRight) && !(transform.rotation.eulerAngles.z > 22 && transform.rotation.eulerAngles.z < 340))
         {
+            Debug.Log(transform.rotation.eulerAngles.z);
             transform.Rotate(Vector3.back * (rotationSpeed * Time.deltaTime));
         }
-        if (Input.GetKey(rotateLeft))
+        if (Input.GetKey(rotateLeft) && !(transform.rotation.eulerAngles.z > 20 && transform.rotation.eulerAngles.z < 338))
         {
+            Debug.Log(transform.rotation.eulerAngles.z);
             transform.Rotate(Vector3.forward * (rotationSpeed * Time.deltaTime));
         }
 
