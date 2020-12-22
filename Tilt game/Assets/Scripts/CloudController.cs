809134a9bda21e7cdcbc6b39a8f90642e9a6db9a
Ben <50188950+ShavedWhale1@users.noreply.github.com>
2020-12-22 12:38:14 -0500

    API update

---
+++ Tilt game/Assets/Scripts/CloudController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudController : MonoBehaviour
+{
+    [Header("Cloud Movement")]
+    [SerializeField] float cloudSpeed;
+    [SerializeField] float speedVariation;
+    [SerializeField] float maxY, minY, startX;
+
+    [SerializeField] GameObject cloudPrefab;
+
+    private float timer = 0f;
+    private float spawnRate = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timer == spawnRate)
+        {
+            GameObject newCloud = Instantiate(cloudPrefab, randomPosition(), Quaternion.identity);
+            newCloud.AddComponent<Rigidbody>();
+            newCloud.transform.position = new Vector3(0.0f, -2.0f, 0.0f);
+            newCloud.rigidbody.velocity = new Vector3(0.0f, 10.0f, 0.0f);
+        }
+    }
+
+    Vector3 randomPosition()
+    {
+        return new Vector3(startX, Random.Range(minY, maxY), 0);
+    }
+}
