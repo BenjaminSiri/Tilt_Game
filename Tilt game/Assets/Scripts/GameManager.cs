a482dfe2f1df4fa77712c0e0cc3776dc33efadda
Ben <ben@acsiri.com>
2021-02-15 20:44:52 -0500

    Same Level problem

--- Tilt game/Assets/Scripts/GameManager.cs
+++ Tilt game/Assets/Scripts/GameManager.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
-    public string[] Scenes = { "Level 1", "Level 2", "Level 3", "Level 4", "Level 5","Level 6", "Level 7"};
+    public string[] Scenes = { "Level 1", "Level 2", "Level 3", "Level 4", "Level 5","Win Scene"};
     public int currentScene;
 
     // Start is called before the first frame update
